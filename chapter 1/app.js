7d855151eb6a8289d3404fd5b98a2f812399283e
Muhammad Anas Raza <anasraza2k6@gmail.com>
2026-03-30 00:20:13 +0500

    chapter 1

---
+++ chapter 1/app.js
@@ -0,0 +1,18 @@
+// chapter # 1
+// Ans 1
+alert("Welcome to our website! We're glad you're here.");
+
+// Ans 2 
+alert("Error! Please enter a valid password.");
+
+// Ans 3
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// Ans 4
+alert("Welcome to JS Land...");
+alert("Happy Coding!\n\nPrevent this page from creating additional dialogs.");
+
+// Ans 5
+// Open browser. Right-click and select Inspect or press F12. Click the Console tab. And type :
+alert("Hello... I can run JS through my web browser's console");
+
