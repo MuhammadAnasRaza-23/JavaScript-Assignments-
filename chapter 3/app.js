39bac42c5cac43d0b708f717680e3acb031599c9
Muhammad Anas Raza <anasraza2k6@gmail.com>
2026-03-30 00:20:54 +0500

    chapter 3

---
+++ chapter 3/app.js
@@ -0,0 +1,21 @@
+// chapter # 3
+// Ans 1
+var age = 15;
+alert("I am " + age + " years old");
+
+// Ans 2
+var visitCount = 14; 
+alert("You have visited this site " + visitCount + " times");
+
+// Ans 3
+var birthYear = 1990;
+document.write("My birth year is " + birthYear + "<br>");
+document.write("Data type of my declared variable is number");
+
+// Ans 4
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
+
