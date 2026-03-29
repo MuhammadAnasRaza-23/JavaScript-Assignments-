c0d365605ebd783148799e592c94f12d749e1509
Muhammad Anas Raza <anasraza2k6@gmail.com>
2026-03-30 00:20:35 +0500

    chapter 2
    JS code

---
+++ chapter 2/app.js
@@ -0,0 +1,35 @@
+// chapter # 2 
+// Ans 1 
+var username; 
+
+// Ans 2
+var myName = "Muhammad Anas Raza";
+
+// Ans 3
+// Part a,b,c
+var message = "Hello World";
+alert(message);
+
+// Ans 4
+var studentName = "Jhone Doe";
+var age = "15 years old"; 
+var course = "Certified Mobile Application Development";
+
+alert(studentName);
+alert(age);
+alert(course);
+
+// Ans 5
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"; 
+alert(pizza);
+
+// Ans 6
+var email = "anasraza2k6@gmail.com"; 
+alert("My email address is " + email); 
+
+// Ans 7
+var book = "A smarter way to learn JavaScript"; 
+alert("I am trying to learn from the Book " + book);
+
+// Ans 8
+document.write("Yah! I can write HTML content through JavaScript");
