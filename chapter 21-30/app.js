0d35ad31520f8946f3e8b920dc1b62ecfbcf17e0
Muhammad Anas Raza <anasraza2k6@gmail.com>
2026-05-11 21:42:29 +0500

    chapters  21-23

---
+++ chapter 21-30/app.js
@@ -0,0 +1,50 @@
+// Question 1 & 2
+// var firstName = prompt("Enter your first name:");
+// var lastName = prompt("Enter your last name:");
+// var fullName = firstName + " " + lastName;
+// alert("Hello, " + fullName + "!");
+
+// Question 3
+// var mobile = prompt("What is your favorite mobile phone model?");
+// document.write("My favorite phone is: " + mobile + "<br>");
+// document.write("Length of string: " + mobile.length);
+
+// Question 4
+// var word = "Pakistani";
+// var index = word.indexOf("n");
+// document.write("String: " + word + "<br>Index of 'n': " + index);
+
+// Question 5
+// var str = "Hello World";
+// var lastIndex = str.lastIndexOf("l");
+// document.write("String: " + str + "<br>Last index of 'l': " + lastIndex);
+
+// Question 6
+// var num = parseFloat(prompt("Enter a positive floating point number:"));
+// document.write("number: " + num + "<br>");
+// document.write("round value: " + Math.round(num) + "<br>");
+// document.write("floor value: " + Math.floor(num) + "<br>");
+// document.write("ceil value: " + Math.ceil(num));
+
+// Question 7
+// var negNum = parseFloat(prompt("Enter a negative floating point number:"));
+// document.write("number: " + negNum + "<br>");
+// document.write("round value: " + Math.round(negNum) + "<br>");
+// document.write("floor value: " + Math.floor(negNum) + "<br>");
+// document.write("ceil value: " + Math.ceil(negNum));
+
+// Question 8
+// var absNum = parseFloat(prompt("Enter a number to find its absolute value:"));
+// document.write("The absolute value of " + absNum + " is " + Math.abs(absNum));
+
+// Question 9
+// var diceValue = Math.floor(Math.random() * 6) + 1;
+// document.write("random dice value: " + diceValue);
+
+// Question 10
+// var toss = Math.floor(Math.random() * 2) + 1;
+// if (toss === 2) {
+//     document.write(toss + "<br>random coin value: Heads");
+// } else {
+//     document.write(toss + "<br>random coin value: Tails");
+// }
